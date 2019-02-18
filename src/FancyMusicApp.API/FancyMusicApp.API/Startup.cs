dcdf6c6a87bb58fe837e2b66b0d18f6d9c34e262
Shkrabo, Evgeniy <eshkrabo@kpmg.ru>
2019-02-18 14:10:20 +0300

    Configured Swagger UI map for API

--- src/FancyMusicApp.API/FancyMusicApp.API/Startup.cs
+++ src/FancyMusicApp.API/FancyMusicApp.API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace FancyMusicApp.API
 {
@@ -23,6 +24,12 @@ namespace FancyMusicApp.API
             services.AddCors();
 
             services.AddTransient<ILibraryService, LibraryService>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Fancy Music App API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +51,16 @@ namespace FancyMusicApp.API
                 .AllowAnyMethod()
                 .AllowCredentials()
             );
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
